4d438a0c2e1db25d7fa510695c3f8a90553c46dd
Trevor Lane <tltrevorlane@gmail.com>
2018-04-16 21:51:03 -0400

    initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+
+public class Main
+{
+    public static final int size = 10;
+    public static void main(String[] args) {
+        // create paired population randomly
+
+        Population pop0 = new Population();
+
+
+    }
+}
---
+++ src/com/company/Population.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class Population {
+  int SIZE = Main.size;
+  Student[] students = new Student[SIZE];
+  int[][] pairs = new int[SIZE/2][2];
+  //SIZE/2 could cause problems, added extra variable just to be safe
+  int pairSize = SIZE/2;
+    double pair[] = new double[pairSize];
+
+
+    //for initial population
+    //initial population
+  public Population()
+  {
+      for(int i = 0; i<SIZE;i++)
+      {
+          students[i] = new Student();
+      }
+
+    //making initial pairs going down list, pretty messing, you can probably do it much simpler
+      for(int i = 0,j=1,k=0; i<pairSize;i++)
+      {
+          pairs[i][0] = k;
+          pairs[i][1] = j;
+          j += 2;
+          k+=2;
+      }
+     //I think that we should have a pair class
+
+//popDiff is total compatability difference
+  double popDiff = 0;
+      for(int i = 0; i< pairSize; i++)
+  {
+      pair[i] = calcFitness(students[pairs[i][0]],students[pairs[i][1]]);
+      popDiff += pair[i];
+  }
+  System.out.println(popDiff);
+  }
+
+
+  public double calcFitness(Student s1,Student s2)
+  {
+      //calculate total distance
+    double sumDiff = 0;
+    for(int i=0;i< s1.answers.length;i++)
+      {
+          double x = s1.answers[i] - s2.answers[i];
+          double diff = Math.abs(x);
+           sumDiff += diff;
+      }
+      return sumDiff;
+  }
+
+
+
+
+
+}
