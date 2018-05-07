53d784a14b820576b5a5505d2fe1d9a9ff3262e2
Raybb <rayz321z@gmail.com>
2018-05-07 11:34:12 -0400

    start multithreading ga runs

---
+++ src/com/company/GeneticAlgoRun.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.Random;
+
+public class GeneticAlgoRun implements Runnable{
+	String DATA_FILE;
+	double MUTATION_PROBABILITY;
+	int POPULATION_SIZE, NUM_GENERATIONS, PRINT_SEPARATION, ELITISM_LEVEL;
+	
+	public GeneticAlgoRun(int POPULATION_SIZE, String DATA_FILE, double MUTATION_PROBABILITY, 
+			int NUM_GENERATIONS, int ELITISM_LEVEL){
+		this.POPULATION_SIZE = POPULATION_SIZE;
+		this.DATA_FILE = DATA_FILE;
+		this.MUTATION_PROBABILITY = MUTATION_PROBABILITY;
+		this.NUM_GENERATIONS = NUM_GENERATIONS;
+		this.ELITISM_LEVEL = ELITISM_LEVEL;
+		
+		
+	}
+
+	public void runGenerations() {
+		// Create the first population
+		Population pop = new Population(POPULATION_SIZE, true);
+
+		for (int i = 0; i < NUM_GENERATIONS; i++) {
+			pop.sortFitnessNew();
+			if (pop.individuals[pop.individuals.length - 1].calculateTotalFitness() == 1.0) {
+				System.out.println("Winner! " + i + " " + pop);
+				break;
+			}
+			pop = createNextGen(pop);
+		}
+
+		System.out.println("Gen last \t" + pop);
+
+	}
+
+	private void mutatexTimes(Individual indiv, int times){
+		for (int a = 0; a < times; a++) indiv.mutate();
+	}
+	
+	private Population createNextGen(Population oldGeneration){
+		Population nextGen = new Population(POPULATION_SIZE, false);
+		for (int j = 0; j < POPULATION_SIZE; j++){
+			nextGen.individuals[j] = new Individual(oldGeneration.selectParent());
+			int r = new Random().nextInt(Main.students.length); // between 0 and number of students - 1
+			mutatexTimes(nextGen.individuals[j], r);
+		}
+		return nextGen;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		runGenerations();
+	}
+	
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,17 @@
 package com.company;
 import java.util.*;
+import java.util.concurrent.*;
 import java.io.*;
 
 public class Main {
-	public static  Student[] students;
+	public static Student[] students;
 
 	/// Configurations/////////////////////////
 
-	static int POPULATION_SIZE = 100;
+	static int POPULATION_SIZE = 30;
 	static String DATA_FILE = "src/com/company/realSampleData.csv";
 	static double MUTATION_PROBABILITY = 1.0/1200.0*3;
-	static int NUM_GENERATIONS = 500000;
+	static int NUM_GENERATIONS = 500;
 	static int PRINT_SEPARATION = 100; // Print every ? generations
 	static int ELITISM_LEVEL = 1; // Select next gen from top ? individuals
 	
@@ -21,50 +22,69 @@ public class Main {
 		} catch (IOException error) {
 			System.out.println(error);
 		}
-		
-		/*Individual aa = new Individual(number_of_students);
-		System.out.println(aa);
-		aa.mutate();
-		System.out.println(aa);
-		System.out.println(aa.calcRoommateCompatibility(students[0], students[1]));
-		*/
-		Population pop = new Population(POPULATION_SIZE, true);
-		System.out.println(pop);
-		//pop.sortByFitness();
-		//System.out.println(pop.selectParent());
-		
-		for (int i = 0; i < NUM_GENERATIONS; i++){
-			Population nextGen = new Population(POPULATION_SIZE, false);
-			pop.sortFitnessNew();
-			if (i%PRINT_SEPARATION == 0) System.out.println("Gen " + i + " " + pop);
-			if(pop.individuals[pop.individuals.length-1].calculateTotalFitness() == 1.0){
-				System.out.println("Winner! " + i + " " + pop);
-				break;
-			}
+		long startTime = System.nanoTime();  
+    
+		ExecutorService executor = Executors.newFixedThreadPool(4);
 
-			/*boolean printDiffs = false;
-			if (i == 1000){
-				printDiffs = true;
-				for (int t = 0; t < pop.individuals.length; t++){
-					System.out.println(pop.individuals[t]);
-				}
-			}*/
-			for (int j = 0; j < POPULATION_SIZE; j++){
-				nextGen.individuals[j] = new Individual(pop.selectParent());
-
-				int r = new Random().nextInt(students.length); // between 0 and number of students - 1
-				//double before = nextGen.individuals[j].calculateTotalFitness();
-				for (int a = 0; a < r; a++){
-					nextGen.individuals[j].mutate();
-				}
-				//double after = nextGen.individuals[j].calculateTotalFitness();
-				
-				//if (printDiffs) System.out.println(before + " " + after);
-			}
-			pop = nextGen;
+		for (int i = 0; i < 10; i++){
+			GeneticAlgoRun a = new GeneticAlgoRun(POPULATION_SIZE, DATA_FILE, MUTATION_PROBABILITY, NUM_GENERATIONS, ELITISM_LEVEL);
+			executor.submit(a);
 		}
 
-		System.out.println("Gen last " + pop);
+		// Need to figure out a way to figure out when all the executors are finished
+		// https://www.programcreek.com/java-api-examples/?class=java.util.concurrent.Executors&method=newWorkStealingPool
+		
+		System.out.println("hi");
+
+		long estimatedTime = System.nanoTime() - startTime;
+		System.out.println(estimatedTime);
+		
+
+		
+//		
+//		/*Individual aa = new Individual(number_of_students);
+//		System.out.println(aa);
+//		aa.mutate();
+//		System.out.println(aa);
+//		System.out.println(aa.calcRoommateCompatibility(students[0], students[1]));
+//		*/
+//		Population pop = new Population(POPULATION_SIZE, true);
+//		System.out.println(pop);
+//		//pop.sortByFitness();
+//		//System.out.println(pop.selectParent());
+//		
+//		for (int i = 0; i < NUM_GENERATIONS; i++){
+//			Population nextGen = new Population(POPULATION_SIZE, false);
+//			pop.sortFitnessNew();
+//			if (i%PRINT_SEPARATION == 0) System.out.println("Gen " + i + " " + pop);
+//			if(pop.individuals[pop.individuals.length-1].calculateTotalFitness() == 1.0){
+//				System.out.println("Winner! " + i + " " + pop);
+//				break;
+//			}
+//
+//			/*boolean printDiffs = false;
+//			if (i == 1000){
+//				printDiffs = true;
+//				for (int t = 0; t < pop.individuals.length; t++){
+//					System.out.println(pop.individuals[t]);
+//				}
+//			}*/
+//			for (int j = 0; j < POPULATION_SIZE; j++){
+//				nextGen.individuals[j] = new Individual(pop.selectParent());
+//
+//				int r = new Random().nextInt(students.length); // between 0 and number of students - 1
+//				//double before = nextGen.individuals[j].calculateTotalFitness();
+//				for (int a = 0; a < r; a++){
+//					nextGen.individuals[j].mutate();
+//				}
+//				//double after = nextGen.individuals[j].calculateTotalFitness();
+//				
+//				//if (printDiffs) System.out.println(before + " " + after);
+//			}
+//			pop = nextGen;
+//		}
+//
+//		System.out.println("Gen last " + pop);
 	}
 
 	public static Student[] createStudents() throws IOException {
